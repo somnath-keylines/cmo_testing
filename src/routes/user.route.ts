6c686a877398e8b242f0058a67618cfa218b9a54
Anandakeylines <Ananda@keylines.net>
2025-09-16 16:41:00 +0530

    feat: add logout functionality with success response

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser, viewProfile } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, viewProfile } from "../controllers/user.controller.js";
 import { authenticateToken } from "../middlewares/Auth.middlewares.js";
 const router = Router();
 
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+// ✅ Logout route
+router.route("/logout").post(authenticateToken, logoutUser);
 router.route("/user-profile").get(authenticateToken,viewProfile)
 
 export default router
